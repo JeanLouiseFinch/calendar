e62b269f05a2713b13ce352f6efa866a2a95a183
JeanLouiseFinch <oputnyi@rambler.ru>
2020-02-07 16:16:18 +0000

    range+test

--- internal/domain/interfaces/storage.go
+++ internal/domain/interfaces/storage.go
@@ -1,18 +1,18 @@
 package interfaces
 
 import (
-	"https://github.com/JeanLouiseFinch/calendar/internal/domain/entities"
 	"context"
-	"errors"
+
+	"github.com/JeanLouiseFinch/calendar/internal/domain/entities"
 )
 
 type Storage interface {
 	AddEvent(ctx context.Context, e *entities.Event) error
-	DeleteEvent(ctx context.Context,id uint) error
-	GetEventByID(ctx context.Context,id uint) (*entities.Event,error)
-	GetEventsByTitle(ctx context.Context, title string) ([]*entities.Event,error)
-	GetEventsByOwner(ctx context.Context, owner string) ([]*entities.Event,error)
-	EditEvent(ctx context.Context,id uint, e *entities.Event) error
-	GetEventsByTimeRange(ctx context.Context,timeRange string) ([]*entities.Event,error)
+	DeleteEvent(ctx context.Context, id uint) error
+	GetEventByID(ctx context.Context, id uint) (*entities.Event, error)
+	GetEventsByTitle(ctx context.Context, title string) ([]*entities.Event, error)
+	GetEventsByOwner(ctx context.Context, owner string) ([]*entities.Event, error)
+	EditEvent(ctx context.Context, id uint, e *entities.Event) error
+	GetEventsByTimeRange(ctx context.Context, timeRange string) error
 	String() string
 }
